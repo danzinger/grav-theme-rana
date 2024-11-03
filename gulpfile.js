cb1d08c02223e23e7f86d3abffd2593f08a55f1b
Matthias Danzinger <hallo@matthiasdanzinger.eu>
2024-11-03 11:17:15 +0100

    fixed unaccesible vars in shorthand properties, worked on hamburgers (unfinished yet)

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,53 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var cleancss = require('gulp-clean-css');
-var csscomb = require('gulp-csscomb');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
+// Import necessary modules
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleancss = require('gulp-clean-css');
+const csscomb = require('gulp-csscomb');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
-// configure the paths
-var watch_dir = './scss/**/*.scss';
-var src_dir = './scss/**/*.scss';
-var dest_dir = './css-compiled';
-
-var paths = {
-    source: src_dir
+// Define paths
+const paths = {
+    source: './scss/**/*.scss',
+    dest: './css-compiled'
 };
 
-function watch() {
-  return gulp.watch(watch_dir, build);
-}
-
-function build() {
-  return gulp.src(paths.source)
-      .pipe(sourcemaps.init())
-      .pipe(sass({
-            outputStyle: 'compressed',
+// Compile SCSS to CSS
+function compileSass() {
+    return gulp.src(paths.source)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'expanded', // Expanded for readability before minification
             precision: 10
-          }).on('error', sass.logError)
-      )
-      .pipe(sourcemaps.write())
-      .pipe(autoprefixer())
-      .pipe(gulp.dest(dest_dir))
-      .pipe(csscomb())
-      .pipe(cleancss())
-      .pipe(rename({
-        suffix: '.min'
-      }))
-      .pipe(gulp.dest(dest_dir));
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(csscomb())
+        .pipe(gulp.dest(paths.dest)) // Unminified output
+        .pipe(cleancss({ level: 2, rebase: false })) // Optimized minification
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.dest))
+        .pipe(browserSync.stream()); // Stream changes to BrowserSync
 }
 
-exports.watch = watch;
-exports.build = build;
-exports.default = build;
+// Serve with BrowserSync and watch for file changes
+function serve() {
+    browserSync.init({
+        proxy: "http://127.0.0.1:8000", // Proxy for localhost
+        notify: false
+    });
+
+    gulp.watch(paths.source, compileSass);
+    gulp.watch('./blueprints/*.yaml').on('change', browserSync.reload);
+    gulp.watch('./templates/**/*.twig').on('change', browserSync.reload);
+    gulp.watch('../../pages/**/*.md').on('change', browserSync.reload);
+    gulp.watch('../../config/**/*.yaml').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+// Define Gulp tasks
+exports.compileSass = compileSass;
+exports.serve = serve;
+exports.default = gulp.series(compileSass, serve);
